be2ce6104ff97c6054e3baea6e605dd4d6d004e3
stefffarias <stefanie.farias4@gmail.com>
2016-05-25 01:31:52 -0430

    agregue cambios en html,js,css e img

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
- $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-  });
-    
-    $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:3
-          },
-          1200:{
-              items:4
-          }
-      }
+$(document).ready(function(){
+
+    $window = $(window);
+    var h = $window.height();
+    $(".contenido-tienda").css("margin-top", h);
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
 })
